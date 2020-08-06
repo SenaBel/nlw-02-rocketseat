241931890fe2d9c2075ed195925d923f4ce873dc
Abel <abelsena1@hotmail.com>
2020-08-06 12:14:33 -0300

    Typing correction

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -143,7 +143,7 @@ const TeacherForm = () => {
                     { value: "0", label: "Domingo" },
                     { value: "1", label: "Segunda-Feira" },
                     { value: "2", label: "Terça-Feira" },
-                    { value: "3", label: "Quartao-Feira" },
+                    { value: "3", label: "Quarta-Feira" },
                     { value: "4", label: "Quinta-Feira" },
                     { value: "5", label: "Sexta-Feira" },
                     { value: "6", label: "Sábado" },
